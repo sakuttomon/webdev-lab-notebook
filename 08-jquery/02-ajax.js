a03fa7943d13ea264ba296b1bd577f4b5185150d
Irvin Lu <ilu@pdx.edu>
2024-02-25 12:35:00 -0800

    jQuery 08 - Removed commented native JavaScript code

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -19,31 +19,6 @@ const addBookToDOM = (item) => {
       .append($("<p>").text(item.released.substr(0, 4)))
       .append($("<p>").text(`${item.numberOfPages} pages`))
   );
-
-  /*
-  let element = document.createElement("div");
-  let title = document.createElement("h4");
-  let author = document.createElement("p");
-  let published = document.createElement("p");
-  let pages = document.createElement("p");
-
-  element.style.display = "flex";
-  element.style.flexDirection = "column";
-  element.style.alignItems = "center";
-  element.style.marginTop = "20px";
-
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
-  published.textContent = item.released.substr(0, 4);
-  pages.textContent = `${item.numberOfPages} pages`;
-
-  element.append(title);
-  element.append(author);
-  element.append(published);
-  element.append(pages);
-
-  app.append(element);
-  */
 };
 
 const fetchData = (url) => {
@@ -68,25 +43,6 @@ const fetchData = (url) => {
       $("#loading").remove();
     },
   });
-
-  /*
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        addBookToDOM(item);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      let li = document.createElement("li");
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
-    })
-    .finally(() => {
-      app.removeChild(loading);
-    });
-  */
 };
 
 fetchData(url);
